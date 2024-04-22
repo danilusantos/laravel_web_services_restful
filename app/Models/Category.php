f98bb30607f6d63be57fe8a773a7fafaad97eb5e
Danilo Santos <danilondosantos@gmail.com>
2024-04-22 15:38:08 -0300

    feat: retornar todos os registros ou a partir de um filtro (index)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    public function getResults($name = null)
+    {
+        if (!$name) {
+            return $this->get();
+        }
+
+        return $this->where('name', 'LIKE', "%{$name}%")->get();
+    }
 }
